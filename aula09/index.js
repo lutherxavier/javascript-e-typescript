7995cb9d383b8ebb8df38d26b6bb0a8b9b44f806
lutherxavier <luthermateus11@gmail.com>
2022-07-22 21:47:22 -0300

    revisao js

--- aula09/index.js
+++ aula09/index.js
@@ -3,3 +3,22 @@
 let contador = 47;
 contador++;
 console.log(contador);
+
+let numero = 2;
+
+numero++;
+console.log(numero);
+numero++;
+console.log(numero);
+numero--;
+console.log(numero);
+numero +=10;
+console.log(numero);
+numero -=4;
+console.log(numero);
+
+let num =  parseFloat('43.76');
+console.log(typeof num);
+console.log(num);
+
+
