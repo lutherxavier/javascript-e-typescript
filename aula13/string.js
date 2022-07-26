65fa0c435ffe06fe073a072f607ea01359b9f662
lutherxavier <luthermateus11@gmail.com>
2022-07-26 19:49:38 -0300

    mais sobre strings

---
+++ aula13/string.js
@@ -0,0 +1,8 @@
+let umaString = "o lalalalalalal\"alallal\"laalalalla";
+console.log(umaString); 
+
+const nome = 'Luther';
+console.log(nome[4]); 
+console.log(nome.indexOf('u',2));
+console.log(nome.length);
+console.log(nome.slice(1, 5));
